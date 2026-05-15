3ea4f33611282869d59056876757e901a891cf74
Fazlı Altun <altunfazl@gmail.com>
2026-05-15 03:00:23 -0400

    Initial Commit

---
+++ Today-Only/Today_OnlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Today_OnlyApp.swift
+//  Today-Only
+//
+//  Created by Fazlı Altun on 15.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct Today_OnlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
